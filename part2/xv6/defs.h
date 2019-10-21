bf18cf33eacbf58fa82fa84067608f77abce32ce
ALPolizzi <apolizzi@hawk.iit.edu>
2019-10-21 15:39:07 -0500

    added getsharedpage and freeshare page syscalls to defs.h and syscall.c

--- part2/xv6/defs.h
+++ part2/xv6/defs.h
@@ -120,7 +120,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-
+int             GetSharedPage(int, int);
+void             FreeSharedPage(int);
 // swtch.S
 void            swtch(struct context**, struct context*);
 
