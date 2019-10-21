30ca67daf2c951f3b2f5516bba965c321b722edc
ALPolizzi <apolizzi@hawk.iit.edu>
2019-10-21 15:52:31 -0500

    changed return type of freesharedpage to int in defs.h

--- part2/xv6/defs.h
+++ part2/xv6/defs.h
@@ -121,7 +121,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             GetSharedPage(int, int);
-void             FreeSharedPage(int);
+int             FreeSharedPage(int);
 // swtch.S
 void            swtch(struct context**, struct context*);
 
