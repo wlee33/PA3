bcb5902c3dad86c159a63ca6825b760234411f3f
ALPolizzi <apolizzi@hawk.iit.edu>
2019-10-28 14:10:35 -0500

    added makefile (should be in orginal state) and sharedmem from spaccesstest branch for merge

--- part2/xv6/sharedmem.c
+++ part2/xv6/sharedmem.c
@@ -5,28 +5,39 @@ int
 main(void)
 {
 	int* sharedpageaddress = (int*)getsharedpage(1,2);
-	*(sharedpageaddress) = 116;
+	*(sharedpageaddress+1) = 8;
+	*sharedpageaddress = 8;
 	//printf(1, "Shared address in parent wiht pid %d is %p,content is %d\n", getpid(),sharedpageaddress,*(sharedpageaddress));
 
-
-	if(fork()==0){
+	//printf(1, "sharedpageaddress contents pre-fork = skip\nAddress is skip\n");
+	//int testint = 0;
+	printf(1, "sharedpageaddress contents pre-fork access number 1 = %d\nAddress is %p\n", *sharedpageaddress, sharedpageaddress);
+	//*sharedpageaddress = 16;	
+	printf(1, "sharedpageaddress contents pre-fork access number 3 = %d\n", *(sharedpageaddress+1));
+	
+	printf(1, "sharedpageaddress contents pre-fork access number 4 = %d\n", *sharedpageaddress);
+	
+	printf(1, "sharedpageaddress contents pre-fork access number 5 = %d\n", *sharedpageaddress);
+	if(fork()==0){/*
 		int* sharedpageaddress2 = (int*)getsharedpage(1,2);
-		if (*sharedpageaddress2 == *sharedpageaddress)
+		if (*sharedpageaddress2 == *sharedpageaddress && *sharedpageaddress==8)
 			printf(1, "Cross proccess communication success 1\ncontent of parent = %d\ncontent of child = %d\n", *sharedpageaddress, *sharedpageaddress2);
 		
 		printf(1, "Hasn't crashed yet1\n");
 		*sharedpageaddress2 = 16;
 
 		printf(1, "Hasn't crashed yet2\n");
-		//printf(1, "Shared address in child with pid %d is: %p\n", getpid(),sharedpageaddress2);
+		printf(1, "Shared address in child with pid %d is: %p\n", getpid(),sharedpageaddress2);
 		//printf(1,"content from first process is : %d (should be 8)\n",*(sharedpageaddress2));
 		freesharedpage(1);
-		exit();
+		*/
+	//	exit();
 	}
 	wait();
 	printf(1, "Hasn't crashed yet3\n");
-	if (*sharedpageaddress == 16)
-		printf(1, "Cross proccess communication success 2\n");
+	//if (*sharedpageaddress == 16)
+		//printf(1, "Cross proccess communication success 2\n");
 
+		printf(1, "post-fork value of sharedpageaddress = %d\n", *sharedpageaddress);
 	return 0;
 }
