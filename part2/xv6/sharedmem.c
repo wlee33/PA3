466f4c5be2747e10c6060a46ded3b49071628002
Jingwen Pickens <jingwenpickens@dhcp40.merunorth98.iit.edu>
2019-10-29 14:56:41 -0500

    different test cases

--- part2/xv6/sharedmem.c
+++ part2/xv6/sharedmem.c
@@ -5,20 +5,24 @@ int
 main(void)
 {
 
-	int* sharedpageaddress = (int*)getsharedpage(2,2);
+	int* sharedpageaddress = (int*)getsharedpage(1,2);
 	*(sharedpageaddress) = 8;
+
 	printf(1, "Shared address in parent wiht pid %d is %p,content is %d\n", getpid(),sharedpageaddress,*(sharedpageaddress));
+	
+	printf(1, "Second time printing! content is %d\n",*(sharedpageaddress));
 
 
 	if(fork()==0){
 
-		int* sharedpageaddress2 = (int*)getsharedpage(1,1);
-		printf(1, "Shared address in child with pid %d is: %p, content is: %d\n", getpid(),sharedpageaddress2, *sharedpageaddress2);
-		//*(sharedpageaddress2) = 10;
+		int* sharedpageaddress2 = (int*)getsharedpage(1,2);
+		int i = *(sharedpageaddress2);
+		printf(1,"Prnting from fork, the content from parent is: %d\n",*(sharedpageaddress2));
+		printf(1,"Printing from fork, copied value i is: %d\n", i);
 		exit();
 	}
-	exit();
+	wait();
 
-	printf(1, "Second time printing: Shared address in parent wiht pid %d is %p,content is %d\n", getpid(),sharedpageaddress,*(sharedpageaddress+4));
+	//printf(1, "Second time printing: Shared address in parent wiht pid %d is %p,content is %d\n", getpid(),sharedpageaddress,*(sharedpageaddress+4));
 	return 0;
 }
